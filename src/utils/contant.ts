d37e725b797acbe43ebed83da79cf5578254524a
TommyH <hiept81331@gmail.com>
2025-11-03 10:10:29 +0700

    sửa lại endpoint cloudflare

--- src/utils/contant.ts
+++ src/utils/contant.ts
@@ -1,3 +1,3 @@
 // export const endpointBe = "http://localhost:3000";
 // export const endpointBe="https://nestjsbemovie.onrender.com"
-export const endpointBe="https://reactmovie-proxy.hiept81331.workers.dev"
+export const endpointBe="https://reactmovie-proxy.hiept81331.workers.dev/api"
